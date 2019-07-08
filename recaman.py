b0f79026790c2a134e5576eeec16be01acfdf325
Masood Qaim <masoodaqaim@gmail.com>
2019-07-07 23:20:56 -0400

    changed pencolor/bgcolor

--- recaman.py
+++ recaman.py
@@ -2,6 +2,7 @@ import turtle
 
 window = turtle.Screen() # create a pop up window
 window.setup(width=800, height=600, startx=10, starty=0.5) # scaling the window
+turtle.bgcolor('black')
 joe = turtle.Turtle() # the turtle object. names is joe
 joe.shape('turtle')
 scale = 5
@@ -21,12 +22,14 @@ for step_size in range(1,100): # joe increases step by 1 each time
     backwards = current - step_size
 
     if backwards > 0 and backwards not in seen:
+        joe.pencolor('red')
         joe.setheading(90) # 90 degrees is pointing straight up
         joe.circle(scale * step_size/2, 180) # 180 degrees means 'draw semicircle'
         current = backwards
         seen.add(current)
 
     else:
+        joe.pencolor('blue')
         joe.setheading(270) # 270 degrees is straight down
         joe.circle(scale * step_size/2, 180)
         current += step_size
