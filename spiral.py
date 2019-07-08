565befb6e07c9541c48dfb89e1617273a7b55f4c
Masood Qaim <masoodaqaim@gmail.com>
2019-07-07 23:05:06 -0400

    added new color

--- spiral.py
+++ spiral.py
@@ -1,6 +1,6 @@
 import turtle
 
-colors = ['red', 'blue', 'green', 'yellow']
+colors = ['red', 'blue', 'green', 'yellow', 'purple']
 
 window = turtle.Screen() # create a pop up window
 window.setup(width=800, height=600, startx=10, starty=0.5) # scaling the window
@@ -12,7 +12,7 @@ joe.speed(0)
 
 # this creates a spiral. joe goes forward by 1 step, then rotates left by 59 degrees 
 for x in range(360):
-    joe.pencolor(colors[x % 4]) # for each line, a new color will be used
+    joe.pencolor(colors[x % 5]) # for each line, a new color will be used
     joe.forward(x)
     joe.left(59)
 
